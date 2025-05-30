033c9a10fef712fe3339e3ddf1271c5c5dfd8847
wnvd <waninaveed998@gmail.com>
2025-05-30 15:49:19 +0530

    added commands exit, help to the repl

--- repl.go
+++ repl.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"strings"
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
+	"strings"
 )
+
+type cliCommand struct {
+	name			string
+	description		string
+	callback		func() error
+}
+
+var keywordParams = map[string]cliCommand{
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: closeRepl,
+	},
+	"help": {
+		name: "help",
+		description:  "Displays help message",
+		callback: helpRepl,
+	},
+}
 
 func startRepl() {
 
@@ -15,7 +34,16 @@ func startRepl() {
 		fmt.Print("Pokedex > ")
 		userInput.Scan()
 		userText := cleanInput(string(userInput.Text()))
-		fmt.Printf("Your command was: %v\n", userText[0])
+
+		if len(userText) == 1 {
+			param := userText[0]
+			command, present := keywordParams[param]
+			if !present {
+				fmt.Println("Unknown command")
+				continue
+			}
+			command.callback()
+		}
 	}
 
 }
@@ -24,4 +52,24 @@ func cleanInput(text string) []string {
 	text = strings.Trim(text, " ")
 	text = strings.ToLower(text)
 	return strings.Split(text, " ")
+}
+
+func closeRepl() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+/* you can range over keywordParams but you have to
+ * use closure for the initialization in the map
+ * without it it causes a cyclic dependency.
+*/ 
+func helpRepl() error {
+	fmt.Println(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex`)
+
+	return nil
 }
