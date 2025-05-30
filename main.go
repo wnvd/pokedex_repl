e602e931f97ca197b60a261866f9c9202727d81e
wnvd <waninaveed998@gmail.com>
2025-05-30 11:54:05 +0530

    refactor

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main () {
-
-	userInput := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		userInput.Scan()
-		userText := cleanInput(string(userInput.Text()))
-		fmt.Printf("Your command was: %v\n", userText[0])
-	}
-
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,24 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func startRepl() {
+
+	userInput := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		userInput.Scan()
+		userText := cleanInput(string(userInput.Text()))
+		fmt.Printf("Your command was: %v\n", userText[0])
+	}
+
+}
 
 func cleanInput(text string) []string {
 	text = strings.Trim(text, " ")
